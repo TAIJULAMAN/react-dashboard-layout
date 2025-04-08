c1a74eedc815ec5b13ec0f876a952c32189f9189
Md Shah Aman Patwary <taijulaman8@gmail.com>
2025-04-08 13:51:40 +0600

    recent user

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,12 +1,79 @@
+import { FaUsers } from "react-icons/fa";
+import { useState } from "react";
+import dayjs from "dayjs";
+import RecentUsers from "./RecentUsers";
 
+function DashboardPage() {
+  const currentYear = dayjs().year();
+  const startYear = 1900;
+  const [selectedYear, setSelectedYear] = useState(currentYear);
+  const [isOpen, setIsOpen] = useState(false);
 
-const Dashboard = () => {
- 
+  const years = Array.from(
+    { length: currentYear - startYear + 1 },
+    (_, index) => startYear + index
+  );
+
+  const handleSelect = (year) => {
+    setSelectedYear(year);
+    setIsOpen(false);
+  };
+
   return (
-    <div className="p-2 min-h-screen">
-      Home page
+    <div className="flex flex-col">
+      <div className="grid grid-cols-1 md:grid-cols-2 mmd:grid-cols-3 lg:grid-cols-5 gap-5">
+        <div className="flex justify-between items-center p-5 bg-[#202020]  gap-5 h-[80px] rounded-lg shadow-md">
+          <div className="flex gap-2 items-center">
+            <p className="rounded-full flex justify-center items-center">
+              <FaUsers size={20} className=" bg-white rounded-full p-2 w-10 h-10 text-[#0B704E]" />
+            </p>
+            <p className="text-xl font-semibold text-white whitespace-nowrap">Total User</p>
+          </div>
+          <p className="text-[#0B704E] text-2xl font-bold">1200</p>
+        </div>
+        <div className="flex justify-between items-center p-5 bg-[#202020]  gap-5 h-[80px] rounded-lg shadow-md">
+          <div className="flex gap-2 items-center">
+            <p className="rounded-full flex justify-center items-center">
+              <FaUsers size={20} className=" bg-white rounded-full p-2 w-10 h-10 text-[#0B704E]" />
+            </p>
+            <p className="text-xl font-semibold text-white whitespace-nowrap">Total Bar</p>
+          </div>
+          <p className="text-[#0B704E] text-2xl font-bold">120</p>
+        </div>
+        <div className="flex justify-between items-center p-5 bg-[#202020]  gap-5 h-[80px] rounded-lg shadow-md">
+          <div className="flex gap-2 items-center">
+            <p className="rounded-full flex justify-center items-center">
+              <FaUsers size={20} className=" bg-white rounded-full p-2 w-10 h-10 text-[#0B704E]" />
+            </p>
+            <p className="text-xl font-semibold text-white whitespace-nowrap">Total Club</p>
+          </div>
+          <p className="text-[#0B704E] text-2xl font-bold">120</p>
+        </div>
+        <div className="flex justify-between items-center p-5 bg-[#202020]  gap-5 h-[80px] rounded-lg shadow-md">
+          <div className="flex gap-2 items-center">
+            <p className="rounded-full flex justify-center items-center">
+              <FaUsers size={20} className=" bg-white rounded-full p-2 w-10 h-10 text-[#0B704E]" />
+            </p>
+            <p className="text-xl font-semibold text-white whitespace-nowrap">Total Restaurant</p>
+          </div>
+          <p className="text-[#0B704E] text-2xl font-bold">120</p>
+        </div>
+        <div className="flex justify-between items-center p-5 bg-[#202020]  gap-5 h-[80px] rounded-lg shadow-md">
+          <div className="flex gap-2 items-center">
+            <p className="rounded-full flex justify-center items-center">
+              <FaUsers size={20} className=" bg-white rounded-full p-2 w-10 h-10 text-[#0B704E]" />
+            </p>
+            <p className="text-xl font-semibold text-white whitespace-nowrap">Total Earning</p>
+          </div>
+          <p className="text-[#0B704E] text-2xl font-bold">120</p>
+        </div>
+      </div>
+      <div className="mt-5">
+        <h1 className="text-2xl font-bold mb-5">Recent Joined User</h1>
+        <RecentUsers />
+      </div>
     </div>
   );
-};
+}
 
-export default Dashboard
+export default DashboardPage;
---
+++ src/components/Dashboard/RecentUsers.jsx
@@ -0,0 +1,103 @@
+import { ConfigProvider, Table } from "antd";
+
+const RecentUsers = () => {
+
+          const dataSource = [
+                    {
+                              key: "1",
+                              no: "1",
+                              name: "John Doe",
+                              date: "05/03/2025",
+                              phone: "+1 9876543210",
+                              email: "johndoe@example.com",
+                              location: "New York, USA",
+                    },
+                    {
+                              key: "2",
+                              no: "2",
+                              name: "Jane Smith",
+                              date: "10/04/2025",
+                              phone: "+44 1234567890",
+                              email: "janesmith@example.com",
+                              location: "London, UK",
+                    },
+                    {
+                              key: "3",
+                              no: "3",
+                              name: "Ali Khan",
+                              date: "15/02/2025",
+                              phone: "+92 3345678901",
+                              email: "alikhan@example.com",
+                              location: "Karachi, Pakistan",
+                    },
+                    {
+                              key: "4",
+                              no: "4",
+                              name: "Emily Davis",
+                              date: "20/05/2025",
+                              phone: "+33 6789012345",
+                              email: "emilydavis@example.com",
+                              location: "Paris, France",
+                    },
+                    {
+                              key: "5",
+                              no: "5",
+                              name: "Michael Brown",
+                              date: "25/06/2025",
+                              phone: "+61 4567890123",
+                              email: "michaelbrown@example.com",
+                              location: "Sydney, Australia",
+                    }
+          ];
+          const columns = [
+                    { title: "No", dataIndex: "no", key: "no" },
+                    { title: "Name", dataIndex: "name", key: "name" },
+                    { title: "Date", dataIndex: "date", key: "date" },
+                    { title: "Phone Number", dataIndex: "phone", key: "phone" },
+                    { title: "Email", dataIndex: "email", key: "email" },
+                    { title: "Location", dataIndex: "location", key: "location" },
+                    {
+                              title: "Action",
+                              key: "action",
+                              render: () => (
+                                        <div className="flex gap-2 text-center py-2 w-full">
+                                                  <button
+                                                            className="bg-[#0DC553] text-white font-semibold py-2 px-5 rounded-lg"
+                                                  >
+                                                            Approve
+                                                  </button>
+                                                  <button
+                                                            className="bg-[#A80608] text-white font-semibold py-2 px-5 rounded-lg"
+                                                  >
+                                                            Cancel
+                                                  </button>
+                                        </div>
+                              ),
+                    },
+          ];
+
+          return (
+                    <>
+                              <ConfigProvider
+                                        theme={{
+                                                  components: {
+                                                            Table: {
+                                                                      headerBg: "#0B704E",
+                                                                      headerColor: "rgb(255,255,255)",
+                                                                      cellFontSize: 16,
+                                                            },
+                                                  },
+                                        }}
+                              >
+                                        <Table
+                                                  dataSource={dataSource}
+                                                  columns={columns}
+                                                  scroll={{ x: "max-content" }}
+                                                  pagination={false}
+                                        />
+                              </ConfigProvider>
+                    </>
+          );
+};
+
+export default RecentUsers;
